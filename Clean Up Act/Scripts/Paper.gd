ef6767aa48063815ede19b6136da406b44f7f23b
tpneumann <tpneumann@uncc.edu>
2020-03-14 17:39:41 -0400

    whacking trash now possible

--- Clean Up Act/Scripts/Paper.gd
+++ Clean Up Act/Scripts/Paper.gd
@@ -2,3 +2,13 @@ extends "res://Scripts/TrashMover.gd"
 
 #NOTHING HERE YET
 #TODO: OVERWRITE hit_left / hit_right
+
+#inherits from TrashMover, should have access to all variables in TrashMover
+
+func hit_left():
+	print("Paper hit left")
+	velocity = Vector2(0, newSpeed).rotated(deg2rad(90))
+
+func hit_right():
+	print("Paper hit right")
+	velocity = Vector2(0, newSpeed).rotated(deg2rad(240))
--- Clean Up Act/Scripts/Plastic.gd
+++ Clean Up Act/Scripts/Plastic.gd
@@ -2,3 +2,16 @@ extends "res://Scripts/TrashMover.gd"
 
 #NOTHING HERE YET
 #TODO: OVERWRITE hit_left / hit_right
+
+#inherits from TrashMover, should have access to all variables in TrashMover
+
+func hit_left():
+	print("Plastic hit left")
+	velocity = Vector2(0, newSpeed).rotated(deg2rad(120))
+
+func hit_right():
+	print("Plastic hit right")
+	velocity = Vector2(0, newSpeed).rotated(deg2rad(270))
+
+func _process(delta):
+	print("bigTest")
